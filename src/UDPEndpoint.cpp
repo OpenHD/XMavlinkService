b9de123d1eb871c2d31b4b0da55719c29dc57ec0
consti10 <geierconstantinabc@gmail.com>
2022-04-14 16:55:33 +0200

    testing

---
+++ src/UDPEndpoint.cpp
@@ -0,0 +1,35 @@
+//
+// Created by consti10 on 14.04.22.
+//
+
+#include "UDPEndpoint.h"
+
+#include <utility>
+
+void UDPEndpoint::sendMessage(MavlinkMessage &message) {
+    debugMavlinkMessage(message.m,"TCPEndpoint::send");
+    if(transmitter!= nullptr){
+        const auto data=message.pack();
+        transmitter->forwardPacketViaUDP(data.data(),data.size());
+    }
+}
+
+void UDPEndpoint::processData(const uint8_t *data, int data_len) {
+    mavlink_message_t msg;
+    for(int i=0;i<data_len;i++){
+        uint8_t res = mavlink_parse_char(MAVLINK_COMM_0, (uint8_t)data[i], &msg, &receiveMavlinkStatus);
+        if (res) {
+            debugMavlinkMessage(msg,"TCPEndpoint::receive");
+            MavlinkMessage message{msg};
+            if(callback!= nullptr){
+                callback(message);
+            }
+        }
+    }
+}
+
+void UDPEndpoint::registerCallback(MAV_MSG_CALLBACK cb) {
+    this->callback=std::move(cb);
+}
+
+
---
+++ src/UDPEndpoint.h
@@ -0,0 +1,43 @@
+//
+// Created by consti10 on 14.04.22.
+//
+
+#ifndef XMAVLINKSERVICE_UDPENDPOINT_H
+#define XMAVLINKSERVICE_UDPENDPOINT_H
+
+#include "Helper.hpp"
+
+
+// Wraps two UDP ports, one for sending and one for receiving data
+// (since TCP and UART for example also allow sending and receiving).
+// If this endpoint shall only send data, set the receive port to -1 and never
+// call sendMessage
+class UDPEndpoint {
+public:
+    UDPEndpoint(const int senderPort,const int receiverPort):SEND_PORT(senderPort),RECV_PORT(receiverPort){
+        if(SEND_PORT>=0){
+            transmitter=std::make_unique<SocketHelper::UDPForwarder>(SocketHelper::ADDRESS_LOCALHOST,SEND_PORT);
+        }
+        if(RECV_PORT>=0){
+            const auto cb=[this](const uint8_t* payload,const std::size_t payloadSize)mutable {
+                this->processData(payload,(int)payloadSize);
+            };
+            receiver=std::make_unique<SocketHelper::UDPReceiver>(SocketHelper::ADDRESS_LOCALHOST,RECV_PORT,cb);
+            receiver->start();
+        }
+    }
+    void sendMessage(MavlinkMessage& message);
+    void registerCallback(MAV_MSG_CALLBACK cb);
+private:
+    void processData(const uint8_t* data,int data_len);
+    std::unique_ptr<SocketHelper::UDPReceiver> receiver;
+    std::unique_ptr<SocketHelper::UDPForwarder> transmitter;
+    const int SEND_PORT;
+    const int RECV_PORT;
+    MAV_MSG_CALLBACK callback=nullptr;
+    mavlink_status_t receiveMavlinkStatus{};
+    std::array<uint8_t,1024> readBuffer{};
+};
+
+
+#endif //XMAVLINKSERVICE_UDPENDPOINT_H
