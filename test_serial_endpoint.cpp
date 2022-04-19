c7884148e0b33e52ce8962e16a1a652f03fd1785
consti10 <geierconstantinabc@gmail.com>
2022-04-19 18:28:15 +0200

    testing

---
+++ test_serial_endpoint.cpp
@@ -0,0 +1,21 @@
+//
+// Created by consti10 on 19.04.22.
+//
+
+#include <iostream>
+
+#include "src/endpoints/SerialEndpoint.h"
+
+int main() {
+    std::cout<< "SerialEndpointTest::start" << std::endl;
+    SerialEndpoint serialEndpoint(SerialEndpoint::TEST_SERIAL_PORT);
+    serialEndpoint.registerCallback([](MavlinkMessage& msg){
+        debugMavlinkMessage(msg.m,"SerialTest");
+    });
+    // now messages should come in. Try disconnecting and reconnecting, and see if messages continue
+    std::this_thread::sleep_for(std::chrono::minutes (5));
+    std::cout<< "SerialEndpointTest::end" << std::endl;
+    return 0;
+}
+
+
