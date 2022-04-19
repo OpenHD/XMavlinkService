3da86410e7720d0188e3e3cdc6c00ebab31874e9
consti10 <geierconstantinabc@gmail.com>
2022-04-20 00:07:06 +0200

    generate lib

--- lib/generate_custom.sh
+++ lib/generate_custom.sh
@@ -1,3 +1,6 @@
 # bin/bash
 
+# Generate our own version of mavlink with custom messages
+
+rm -rf c_library_v2_openhd
 python3 mavlink/pymavlink/tools/mavgen.py --lang=C --wire-protocol=2.0 --output=c_library_v2_openhd mavlink/message_definitions/v1.0/openhd.xml
