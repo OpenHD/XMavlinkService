c43807c84065a66a95cd32d77ee0b086f21bc726
consti10 <geierconstantinabc@gmail.com>
2022-04-28 11:17:01 +0200

    add mavlink log message

--- src/ohd_telemetry/InternalTelemetry.cpp
+++ src/ohd_telemetry/InternalTelemetry.cpp
@@ -96,8 +96,8 @@ void InternalTelemetry::processLogMessageData(const uint8_t* data,std::size_t da
         memcpy((uint8_t*)&local_message, data, dataLen);
         // check if the string has a null-terminator
         bool nullTerminatorFound=false;
-        for(int i=0;i<50;i++){
-            if(local_message.message[i]=='\0'){
+        for(auto i : local_message.message){
+            if(i=='\0'){
                 nullTerminatorFound= true;
                 break;
             }
